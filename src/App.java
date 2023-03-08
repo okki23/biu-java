11f833fd319e256c706350a347f2cca8151fd0d3
okki23 <okkisetyawan@gmail.com>
2023-03-08 19:01:46 +0700

    update pertama

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String nama;
+        int usia;
+        nama = "Okki Setyawan";
+        usia = 30;
+        System.out.println("Perkenalkan nama saya : "+nama);
+        System.out.println("Usia saya : "+usia+ " Tahun");
+    }
+
+
+}
